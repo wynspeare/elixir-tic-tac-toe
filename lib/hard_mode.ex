9545b360e843d4168e31ddf65b0d96176b78f6e1
wynspeare <caroline@8thlight.com>
2019-08-08 15:22:01 -0500

    Revised hard_mode with extracted minimax steps

--- lib/hard_mode.ex
+++ lib/hard_mode.ex
@@ -5,159 +5,42 @@ defmodule Hard_Mode do
     %Hard_Mode{marker: marker}
   end
 
-
-  def get_minimizing_player(game, marker) do
-    if marker == game.player_one.marker do
-      game.player_two.marker
-    else
-      game.player_one.marker
-    end
-  end
-
-  def score(board, marker, {max_player, min_player}, depth) do
+  def score(board, marker, {max_player, min_player}) do
     cond do
-      Rules.is_won(board, marker) and marker == max_player -> 10 - depth
-      Rules.is_won(board, marker) and marker == min_player -> depth - 10
+      Rules.is_won(board, marker) and marker == max_player -> 10
+      Rules.is_won(board, marker) and marker == min_player -> -10
       true -> 0
     end
   end
 
-  def minimax(board, current, {max_player, min_player}, depth \\ 0) do
+  def next_boards(board, {max_player, _min_player}) do
+    available_moves = Board.available_cells(board)
 
-      available_moves = Board.available_cells(board)
-
-      Enum.map(available_moves, fn move ->
-        potential_board = Board.place_marker(move, board, current)
-
-        {minimax(potential_board, current, {max_player, min_player}, depth, move), move}
-      end)
-      |> get_best_score(max_player)
+    Enum.map(available_moves, fn move ->
+      {Board.place_marker(move, board, max_player), move}
+    end)
   end
 
-  def minimax(board, current, {max_player, min_player}, depth, move) do
-    Console.display_board(board)
+  def score_next_boards(next_boards, current, {max_player, min_player}) do
+    next_boards
+    |> Enum.map(fn {board, move} ->
+      {score(board, current, {max_player, min_player}), move}
+    end)
+  end
 
-    if Rules.is_over(board, current) do
-      IO.puts("GAME IS OVER - draw or #{current} has won.")
-      IO.puts("Score: #{score(board, current, {max_player, min_player}, depth)}")
-      score(board, current, {max_player, min_player}, depth)
+  def get_best(scores_and_moves) do
+    scores_and_moves
+    |> Enum.max()
+    |> elem(1)
+  end
+
+  def switch_player(current_player_marker, {max_player, min_player}) do
+    IO.puts("switching!")
+
+    if current_player_marker == max_player do
+      min_player
     else
-      depth = depth + 1
-      current = switch_player(current, {max_player, min_player})
-      minimax(board, current, {max_player, min_player}, depth)
+      max_player
     end
   end
-
-  def get_best_score(scores, max_player) do
-    scores
-
-    # if max_player == :marker do
-    #   Enum.max(scores)
-    #   |> elem(1)
-    # else
-    #   Enum.min(scores)
-    #   |> elem(1)
-    # end
-  end
-
-
-      def switch_player(current_player_marker, {max_player, min_player}) do
-        IO.puts("switching!")
-        if current_player_marker == max_player do
-          min_player
-        else
-          max_player
-        end
-      end
-
-
 end
-
-
-
-# defmodule Hard_Mode do
-#   defstruct [:marker]
-
-#   def build(marker) do
-#     %Hard_Mode{marker: marker}
-#   end
-
-
-#   def get_minimizing_player(game, marker) do
-#     if marker == game.player_one.marker do
-#       game.player_two.marker
-#     else
-#       game.player_one.marker
-#     end
-#   end
-
-#       def score(board, marker, {max_player, min_player}) do
-#         cond do
-#           Rules.is_won(board, marker) and marker == max_player -> 10
-#           Rules.is_won(board, marker) and marker == min_player -> -10
-#           true -> 0
-#         end
-#       end
-
-
-#   def minimax(board, {max_player, min_player}) do
-#     if Rules.is_over(board, min_player) do
-#       score(board, min_player, {max_player, min_player})
-#     end
-#       scores = []
-#       moves = []
-#       available_moves = Board.available_cells(board)
-
-#       minimax(available_moves, max_player, {max_player, min_player}, [scores], [moves], board, board)
-#   end
-
-
-#   def minimax([head | tail], current, {max_player, min_player}, [scores], [moves], board, original_board) do
-
-#     potential_board = Board.place_marker(head, board, current) #Place marker for O max player space 2
-#     IO.puts("Potential Board - Cell: #{head} Player: #{current}")
-#     IO.puts("Score: #{score(potential_board, current, {max_player, min_player})}")
-#     Console.display_board(potential_board)
-
-#     if Rules.is_over(potential_board, current) do
-#       IO.puts("GAME IS OVER - draw or #{current} has won.")
-#       IO.puts("SAVE score and move for this end state. Call minimax on next available space on same board.")
-
-#       minimax(tail, current, {max_player, min_player}, [scores ++ [score(potential_board, current, {max_player, min_player})]], [moves ++ [head]], board, original_board)
-
-
-#     else
-
-#       IO.puts("Game NOT over - #{current} has not won. Switch players and call minimax on next available space on potential board")
-
-#       current = switch_player(current, {max_player, min_player})
-#       available_moves = Board.available_cells(potential_board)
-
-#       minimax(available_moves, current, {max_player, min_player}, [scores], [moves], potential_board, original_board)
-#     end
-#   end
-
-
-#   def minimax([], _, {max_player, _min_player}, [scores], [moves], _, _) do
-#     # if max_player == :marker do
-#       Enum.zip(scores, moves)
-#       #   |> Enum.max()
-#       #   |> elem(1)
-#       # else
-#       #     Enum.zip(scores, moves)
-#       #     |> Enum.min()
-#       #     |> elem(1)
-#       #   end
-#       end
-
-#       def switch_player(current_player_marker, {max_player, min_player}) do
-#         IO.puts("switching!")
-#         if current_player_marker == max_player do
-#           min_player
-#         else
-#           max_player
-#         end
-#       end
-
-
-# end
--- test/hard_mode_test.exs
+++ test/hard_mode_test.exs
@@ -1,60 +1,64 @@
 defmodule Hard_ModeTest do
   use ExUnit.Case
-  # import ExUnit.CaptureIO
 
-  # test "score is zero for an empty board" do
-  #   board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  test "score is zero for an empty board" do
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-  #   assert Hard_Mode.score(board, "X", {"O", "X"}) == 0
-  # end
-
-  # test "score is zero for a unfinished board" do
-  #   board = ["X", 2, 3, "O", 5, 6, 7, 8, 9]
-
-  #   assert Hard_Mode.score(board, "X", {"O", "X"}) == 0
-  # end
-
-  # test "score takes in a board and current player marker and is zero for a drawn game" do
-  #   board = ["X", "X", "O", "O", "O", "X", "X", "O", "X"]
-
-  #   assert Hard_Mode.score(board, "X", {"O", "X"}) == 0
-  # end
-
-
-  # test "score takes in a board and current player marker and is 10 for a won game" do
-  #   board = ["O", "X", 3, "X", "O", 6, "X", 8, "O"]
-
-  #   assert Hard_Mode.score(board, "O", {"O", "X"}) == 10
-  # end
-
-  # test "score takes in a board and a marker and is -10 for a lost game" do
-  #   board = ["X", "O", 3, "O", "X", 6, "O", 8, "X"]
-  #   # board = ["O", "X", 3, "X", "O", 6, "X", 8, "O"]
-
-  #   assert Hard_Mode.score(board, "X", {"O", "X"}) == -10
-  # end
-
-  # test "get_minimizing_player takes in a game and a marker and provides the opposite player" do
-  #   game = %Game{
-  #     board: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  #     player_one: %Human{marker: "X"},
-  #     player_two: %Human{marker: "O"},
-  #     current_player: %Human{marker: "X"}
-  #   }
-  #   assert Hard_Mode.get_minimizing_player(game, "X") == "O"
-  # end
-
-
-  test "hard mode can select the best move a partially filled board" do
-    board = ["X", 2, "O", "O", 5, 6, "O", "X", "X"]
-
-    assert Hard_Mode.minimax(board, "O", {"O", "X"}) == 5
+    assert Hard_Mode.score(board, "X", {"O", "X"}) == 0
   end
 
-  # test "7 hard mode can select the best move a partially filled board" do
-  #   board = ["X", "O", "X", 4, "X", 6, 7, 8, "O"]
+  test "score is zero for a unfinished board" do
+    board = ["X", 2, 3, "O", 5, 6, 7, 8, 9]
 
-  #   assert Hard_Mode.minimax(board, {"O", "X"}) == 7
-  # end
+    assert Hard_Mode.score(board, "X", {"O", "X"}) == 0
+  end
 
+  test "score takes in a board and current player marker and is zero for a drawn game" do
+    board = ["X", "X", "O", "O", "O", "X", "X", "O", "X"]
+
+    assert Hard_Mode.score(board, "X", {"O", "X"}) == 0
+  end
+
+  test "score takes in a board and current player marker and is 10 for a won game" do
+    board = ["O", "X", 3, "X", "O", 6, "X", 8, "O"]
+
+    assert Hard_Mode.score(board, "O", {"O", "X"}) == 10
+  end
+
+  test "score takes in a board and a marker and is -10 for a lost game" do
+    board = ["X", "O", 3, "O", "X", 6, "O", 8, "X"]
+
+    assert Hard_Mode.score(board, "X", {"O", "X"}) == -10
+  end
+
+  test "next_boards returns all potential next boards" do
+    board = ["X", 2, "O", "O", 5, "X", "O", "X", "X"]
+
+    assert Hard_Mode.next_boards(board, {"O", "X"}) ==
+             [
+               {["X", "O", "O", "O", 5, "X", "O", "X", "X"], 2},
+               {["X", 2, "O", "O", "O", "X", "O", "X", "X"], 5}
+             ]
+  end
+
+  test "score_next_boards takes potential boards returns moves with scores" do
+    next_boards = [
+      {["X", "O", "O", "O", 5, "X", "O", "X", "X"], 2},
+      {["X", 2, "O", "O", "O", "X", "O", "X", "X"], 5}
+    ]
+
+    assert Hard_Mode.score_next_boards(next_boards, "O", {"O", "X, 5"}) == [{0, 2}, {10, 5}]
+  end
+
+  test "get_best returns the move with the highest score from 2 element list" do
+    scores_and_moves = [{0, 2}, {10, 5}]
+
+    assert Hard_Mode.get_best(scores_and_moves) == 5
+  end
+
+  test "get_best returns the move with the highest score from 5 element list" do
+    scores_and_moves = [{0, 2}, {10, 5}, {-20, 6}, {10, 3}, {9, 9}]
+
+    assert Hard_Mode.get_best(scores_and_moves) == 5
+  end
 end
