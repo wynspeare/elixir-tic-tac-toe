c1f75cd142ecd7deeb88ba48746b9745469448b7
wynspeare <caroline@8thlight.com>
2019-08-09 11:16:18 -0500

    Minimax successfully returning best score

--- lib/hard_mode.ex
+++ lib/hard_mode.ex
@@ -5,84 +5,48 @@ defmodule Hard_Mode do
     %Hard_Mode{marker: marker}
   end
 
-  def score(board, marker, {max_player, min_player}) do
+  def score(board, marker, max_player, min_player, depth \\ 0) do
     cond do
-      Rules.is_won(board, marker) and marker == max_player -> 10
-      Rules.is_won(board, marker) and marker == min_player -> -10
+      Rules.is_won(board, marker) and marker == max_player -> 10 - depth
+      Rules.is_won(board, marker) and marker == min_player -> depth - 10
       true -> 0
     end
   end
 
-  def minimax(board, current, {max_player, min_player}) do
-    next_boards(board, {max_player, min_player})
-    |> score_next_boards(current, {max_player, min_player})
-    |> next_level(current, {max_player, min_player})
-    # |> get_best()
-  end
+  def get_best_move(board, current, max_player, min_player, over \\ false, scores \\ [], depth \\ 0)
+  def get_best_move(board, current, max_player, min_player, true, _scores, depth), do: score(board, current, max_player, min_player, depth)
+  def get_best_move(board, current, max_player, min_player, false, _scores, depth) do
+    scores =
+    Enum.map(board |> Board.available_cells, fn(move) ->
 
-  def next_boards(board, {max_player, _min_player}) do
-    available_moves = Board.available_cells(board)
+      potential_board = Board.place_marker(move, board, current)
+      current = switch_player(current, max_player, min_player)
 
-    Enum.map(available_moves, fn move ->
-      {Board.place_marker(move, board, max_player), move}
+      score = if depth < 5, do: get_best_move(potential_board, current, max_player, min_player, Rules.is_over(potential_board, current), [], depth + 1)
+      {move, score}
     end)
+    if depth == 0, do: best_move(scores), else: minimax_score(max_player, scores)
   end
 
-  def score_next_boards(next_boards, current, {max_player, min_player}) do
-    next_boards
-    |> Enum.map(fn {board, move} ->
-      if is_list(board) do
-        if Rules.is_over(board, current) do
-          {score(board, current, {max_player, min_player}), move}
-        else
-          {score(board, current, {max_player, min_player}), board, move}
-        end
-      end
-    end)
+  def minimax_score(max_player, scores) do
+    if max_player == :marker do
+      scores
+      |> Enum.max_by(fn({_move, score}) -> score end)
+      |> elem(1)
+    else
+      scores
+      |> Enum.min_by(fn({_move, score}) -> score end)
+      |> elem(1)
+    end
   end
 
-  def next_level(tree, current, {max_player, min_player}) do
-    tree
-    |> Enum.map(fn potential_board ->
-      if is_list(elem(potential_board, 1)) do
-
-        current = switch_player(current, {max_player, min_player})
-
-        elem(potential_board, 1)
-
-        |> next_boards({min_player, max_player})
-        |> score_next_boards(current, {max_player, min_player}, elem(potential_board, 2))
-      else
-        potential_board
-      end
-    end)
-    |> List.flatten()
-    |> get_best()
-
+  def best_move(scores) do
+    scores
+    |> Enum.max_by(fn({_move, score}) -> score end)
+    |> elem(0)
   end
 
-  def score_next_boards(next_boards, current, {max_player, min_player}, original_move) do
-    next_boards
-    |> Enum.map(fn {board, _} ->
-      if is_list(board) do
-        if Rules.is_over(board, current) do
-          {score(board, current, {max_player, min_player}), original_move}
-        else
-          [{score(board, current, {max_player, min_player}), board, original_move}]
-          |> next_level(current, {max_player, min_player})
-        end
-      end
-    end)
-  end
-
-  def get_best(scores_and_moves) do
-    scores_and_moves
-    |> Enum.max()
-    |> elem(1)
-  end
-
-
-  def switch_player(current_player_marker, {max_player, min_player}) do
+  def switch_player(current_player_marker, max_player, min_player) do
     if current_player_marker == max_player do
       min_player
     else
@@ -90,3 +54,4 @@ defmodule Hard_Mode do
     end
   end
 end
+
--- test/hard_mode_test.exs
+++ test/hard_mode_test.exs
@@ -4,113 +4,78 @@ defmodule Hard_ModeTest do
   test "score is zero for an empty board" do
     board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    assert Hard_Mode.score(board, "X", {"O", "X"}) == 0
+    assert Hard_Mode.score(board, "X", "O", "X") == 0
   end
 
   test "score is zero for a unfinished board" do
     board = ["X", 2, 3, "O", 5, 6, 7, 8, 9]
 
-    assert Hard_Mode.score(board, "X", {"O", "X"}) == 0
+    assert Hard_Mode.score(board, "X", "O", "X") == 0
   end
 
   test "score takes in a board and current player marker and is zero for a drawn game" do
     board = ["X", "X", "O", "O", "O", "X", "X", "O", "X"]
 
-    assert Hard_Mode.score(board, "X", {"O", "X"}) == 0
+    assert Hard_Mode.score(board, "X", "O", "X") == 0
   end
 
   test "score takes in a board and current player marker and is 10 for a won game" do
     board = ["O", "X", 3, "X", "O", 6, "X", 8, "O"]
 
-    assert Hard_Mode.score(board, "O", {"O", "X"}) == 10
+    assert Hard_Mode.score(board, "O", "O", "X") == 10
   end
 
   test "score takes in a board and a marker and is -10 for a lost game" do
     board = ["X", "O", 3, "O", "X", 6, "O", 8, "X"]
 
-    assert Hard_Mode.score(board, "X", {"O", "X"}) == -10
+    assert Hard_Mode.score(board, "X", "O", "X") == -10
   end
 
-  test "next_boards returns all potential next boards" do
+  test "get_best_move takes in a board with two spaces left and returns the best possible move" do
     board = ["X", 2, "O", "O", 5, "X", "O", "X", "X"]
 
-    assert Hard_Mode.next_boards(board, {"O", "X"}) ==
-             [
-               {["X", "O", "O", "O", 5, "X", "O", "X", "X"], 2},
-               {["X", 2, "O", "O", "O", "X", "O", "X", "X"], 5}
-             ]
+    assert Hard_Mode.get_best_move(board, "O", "O", "X") == 5
   end
 
-  test "score_next_boards takes potential boards returns moves with scores" do
-    next_boards = [
-      {["X", "O", "O", "O", 5, "X", "O", "X", "X"], 2},
-      {["X", 2, "O", "O", "O", "X", "O", "X", "X"], 5}
-    ]
-
-    assert Hard_Mode.score_next_boards(next_boards, "O", {"O", "X"}) == [{0, ["X", "O", "O", "O", 5, "X", "O", "X", "X"], 2}, {10, 5}]
-  end
-
-  test "get_best returns the move with the highest score from 2 element list" do
-    scores_and_moves = [{0, 2}, {10, 5}]
-
-    assert Hard_Mode.get_best(scores_and_moves) == 5
-  end
-
-  test "get_best returns the move with the highest score from 5 element list" do
-    scores_and_moves = [{0, 2}, {10, 5}, {-20, 6}, {10, 3}, {9, 9}]
-
-    assert Hard_Mode.get_best(scores_and_moves) == 5
-  end
-
-  test "minimax takes in a board with two spaces left and returns the best possible move" do
-    board = ["X", 2, "O", "O", 5, "X", "O", "X", "X"]
-
-    assert Hard_Mode.minimax(board, "O", {"O", "X"}) == 5
-  end
-
-  test "minimax takes in a board with 3 spaces left and returns the best possible move" do
+  test "get_best_move takes in a board with 3 spaces left and returns the best possible move" do
     board = ["X", 2, "O", "O", 5, 6, "O", "X", "X"]
 
-    assert Hard_Mode.minimax(board, "O", {"O", "X"}) == 5
+    assert Hard_Mode.get_best_move(board, "O", "O", "X") == 5
   end
 
-  #   test "minimax takes in a board with 4 spaces left and returns the best possible move" do
-  #   board = ["X", "O", "X", 4, "X", 6, 7, 8, "O"]
-
-  #   assert Hard_Mode.minimax(board, "O", {"O", "X"}) == 7
-  # end
-
-  test "next level takes in a tree and players and move and calculates boards?" do
-    tree = [
-      {0, ["X", "O", "O", "O", 5, 6, "O", "X", "X"], 2},
-      {10, 5},
-      {0, ["X", 2, "O", "O", 5, "O", "O", "X", "X"], 6}
-    ]
-    assert Hard_Mode.next_level(tree, "O", {"O", "X"}) == 5
-  end
-
-  test "get next board scores for 4 spaces" do
+  test "get_best_move takes in a board with 4 spaces left and returns the best possible move" do
     board = ["X", "O", "X", 4, "X", 6, 7, 8, "O"]
 
-    # assert Hard_Mode.minimax(board, "O", {"O", "X"}) == 7
-    assert Hard_Mode.next_boards(board, {"O", "X"}) |> Hard_Mode.score_next_boards("O", {"O", "X"}) == [
-      {0, ["X", "O", "X", "O", "X", 6, 7, 8, "O"], 4},
-      {0, ["X", "O", "X", 4, "X", "O", 7, 8, "O"], 6},
-      {0, ["X", "O", "X", 4, "X", 6, "O", 8, "O"], 7},
-      {0, ["X", "O", "X", 4, "X", 6, 7, "O", "O"], 8}
-    ]
+    assert Hard_Mode.get_best_move(board, "O", "O", "X") == 7
   end
 
-  # test "get next level scores for 4 spaces" do
-  #   tree = [
-  #     {0, ["X", "O", "X", "O", "X", 6, 7, 8, "O"], 4},
-  #     {0, ["X", "O", "X", 4, "X", "O", 7, 8, "O"], 6},
-  #     {0, ["X", "O", "X", 4, "X", 6, "O", 8, "O"], 7},
-  #     {0, ["X", "O", "X", 4, "X", 6, 7, "O", "O"], 8}
-  #   ]
+  test "get_best_move takes in a board with 5 spaces left and returns the best possible move" do
+    board = ["X", 2, "O", 4, "X", 6, 7, 8, "O"]
 
-  #   # assert Hard_Mode.minimax(board, "O", {"O", "X"}) == 7
-  #   assert Hard_Mode.next_level(tree, "O", {"O", "X"}) == 5
-  # end
+    assert Hard_Mode.get_best_move(board, "X", "X", "O") == 6
+  end
+
+  test "get_best_move takes in a board with 6 spaces left and returns the best possible move" do
+    board = [1, 2, 3, "X", "X", 6, "O", 8, 9]
+
+    assert Hard_Mode.get_best_move(board, "O", "O", "X") == 6
+  end
+
+  test "get_best_move takes in a board with 7 spaces left and returns the best possible move" do
+    board = [1, 2, 3, 4, 5, 6, "O", 8, "X"]
+
+    assert Hard_Mode.get_best_move(board, "X", "X", "O") == 1
+  end
+
+  test "get_best_move takes in a board with 8 spaces left and returns the best possible move" do
+    board = [1, 2, 3, 4, 5, 6, 7, 8, "X"]
+
+    assert Hard_Mode.get_best_move(board, "O", "O", "X") == 1
+  end
+
+  test "get_best_move takes in an empty board and returns the best possible move" do
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    assert Hard_Mode.get_best_move(board, "O", "O", "X") == 1
+  end
 end
-
